a6495e27261aad18867c579c81ef6bc7dd9fddc7
gabriely <gabriely_maciel@outlook.com>
2022-07-27 16:44:55 -0300

    primeiro commit

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,66 @@
+<template>
+  <header>
+    <nav>
+      <div class="logo">
+        <router-link to="/">Techno</router-link>
+      </div>
+      <ul>
+        <li>
+          <router-link to="/cursos">Cursos</router-link>
+        </li>
+        <li>
+          <router-link to="/contato">Contato</router-link>
+        </li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+    name: 'HeaderComponent'
+}
+</script>
+
+
+<style scoped>
+/* scoped para nao vazar, quer dizer que esse css so vai ficar nesse component e nao aplicar no restante  */
+header {
+  background: #234;
+}
+
+header a {
+  color: white;
+}
+
+a.router-link-active {
+  color: #4b8;
+}
+
+nav {
+  max-width: 800px;
+  margin: 0 auto;
+  display: flex;
+  align-content: space-between;
+  align-items: center;
+  padding: 20px;
+}
+
+.logo {
+  flex: 1;
+}
+
+.logo a {
+  font-size: 1.2rem;
+  color: white;
+}
+
+ul li {
+  display: inline-block;
+}
+
+li a {
+  padding: 0 10px;
+  margin-left: 10px;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import LoadingComponent from './components/LoadingComponent'
 
 Vue.config.productionTip = false
+
+Vue.component("LoadingComponent", LoadingComponent);
 
 new Vue({
   router,
